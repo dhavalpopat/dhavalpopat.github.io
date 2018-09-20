66e9c6f36fe913b1bcc3a957b249c24a1eaea3fb
Dhaval Popat <dhavalpopat10@gmail.com>
2018-09-20 01:00:21 -0400

    Added parallax scrolling

--- js/site.js
+++ js/site.js
@@ -1,2 +1,31 @@
 // var windowWidth = $(window).width();
 // var windowHeight = $(window).height();
+
+/********** image slideshow **********/
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function nextSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("image-slider");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1} 
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none"; 
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block"; 
+//   dots[slideIndex-1].className += " active";
+// }
