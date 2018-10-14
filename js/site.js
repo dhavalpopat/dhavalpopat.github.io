df1e07f40d57622af2db4ba371253872e2b5b65c
Dhaval Popat <dhavalpopat10@gmail.com>
2018-10-13 21:32:25 -0400

    Added scroll up anchor

--- js/site.js
+++ js/site.js
@@ -1,6 +1,26 @@
 // var windowWidth = $(window).width();
 // var windowHeight = $(window).height();
 
+window.onscroll = function() {scrollFunction();};
+
+function scrollFunction() {
+    // show the button when user scrolls down 100px from top of the document
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("scroll-up-anchor").style.display = "block";
+    } else {
+        document.getElementById("scroll-up-anchor").style.display = "none";
+    }
+}
+
+// scroll to top of the document when user clicks on the button
+function takeMeUp() {
+    window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth' 
+    });
+}
+
 /********** image slideshow **********/
 
 // var slideIndex = 1;
